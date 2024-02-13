a5441b3d6062eac82f925beaab6490728ae37b9b
Vineela Gudipati <vineelagudipati@Vineelas-Air.lan>
2024-02-13 02:58:17 -0500

    white noise

--- dashboard/settings.py
+++ dashboard/settings.py
@@ -53,7 +53,8 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "whitenoise.middleware.whiteNoiseMiddleware"
+    "whitenoise.middleware.WhiteNoiseMiddleware"
+    
 ]
 
 ROOT_URLCONF = "dashboard.urls"
